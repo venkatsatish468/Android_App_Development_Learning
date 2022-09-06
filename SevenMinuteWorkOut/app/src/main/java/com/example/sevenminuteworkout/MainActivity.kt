c08bffcf9416ca0b4bea971f6cfdd597927aa9de
A <VA4991@zebra.com>
2022-09-06 17:58:25 +0530

    Completed with the custom progress bar

--- SevenMinuteWorkOut/app/src/main/java/com/example/sevenminuteworkout/MainActivity.kt
+++ SevenMinuteWorkOut/app/src/main/java/com/example/sevenminuteworkout/MainActivity.kt
@@ -1,22 +1,37 @@
 package com.example.sevenminuteworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.sevenminuteworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton: FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener {
-            Toast.makeText(
-                this,
-                "Button Clicked",
-                Toast.LENGTH_SHORT
-            ).show()
+        binding?.flStart?.setOnClickListener {
+            val intent = Intent(this,ExerciseActivity::class.java)
+            startActivity(intent)
+
         }
+
+//        val flStartButton: FrameLayout = findViewById(R.id.flStart)
+//        flStartButton.setOnClickListener {
+//            Toast.makeText(
+//                this,
+//                "Button Clicked",
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
